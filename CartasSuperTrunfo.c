ff5bb4534675b826af59b518e7879ec8fa1fac82
erickbatatom-droid <erickbatatom@gmail.com>
2026-05-11 17:53:51 +0000

    tema3_nv-aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,145 +6,186 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char estado1, estado2;
-char codigo1[3], codigo2[3];
-char nome_cidade1[20], nome_cidade2[20];
-int populacao1, populacao2;
-int P_turistico1, P_turistico2; 
-float area1, area2;
-float PIB1, PIB2;
-float densidade_populacional1, densidade_populacional2;
-float PIB_per_capita1, PIB_per_capita2;
-float superpoder1, superpoder2;
-int escolha_comparacao;
+  char estado1, estado2;
+  char codigo1[3], codigo2[3];
+  char nome_cidade1[20], nome_cidade2[20];
+  int populacao1, populacao2;
+  int P_turistico1, P_turistico2; 
+  float area1, area2;
+  float PIB1, PIB2;
+  float densidade_populacional1, densidade_populacional2;
+  float PIB_per_capita1, PIB_per_capita2;
+  float superpoder1, superpoder2;
+  int escolha_comparacao;
 
-//area de entrada de dados
-//leitura dos dados da primeira carto
+  //area de entrada de dados
+  //leitura dos dados da primeira carto
 
-printf("Letra do estado: ");
-scanf("%c", &estado1); //letra do estado
-                                  
-printf("Código do estado: ");
-scanf("%s", codigo1); //codigo do estado
-                                          
-printf("Nome da cidade: ");
-scanf("%s", nome_cidade1); //nome da cidade
+  printf("Letra do estado: ");
+  scanf("%c", &estado1); //letra do estado
 
-printf("População: ");
-scanf("%d", &populacao1); //populacao da cidade
-                                                           
-printf("Área: ");
-scanf("%f", &area1); //area da cidade
-                                                                   
-printf("PIB: ");
-scanf("%f", &PIB1); //PIB da cidade
-                                                                         
-printf("Pontos turísticos: ");
-scanf("%d", &P_turistico1); //pontos turisticos
+  printf("Código do estado: ");
+  scanf("%s", codigo1); //codigo do estado
 
-printf("\n"); //linha em branco para separar as cartas
+  printf("Nome da cidade: ");
+  scanf("%s", nome_cidade1); //nome da cidade
 
-//leitura dos dados da segunda carta
-printf("segunda carta\n");
-printf("Letra do estado: ");
-scanf(" %c", &estado2); //letra do state
-                                                                                           
-printf("Código do estado: ");
-scanf("%s", codigo2); //codigo do estado
-                                                                                                  
-printf("Nome da cidade: ");
-scanf("%s", nome_cidade2); //nome da cidade
-                                                                                                          
-printf("População: ");
-scanf("%d", &populacao2); //populacao da cidade
-                                                                                                                  
-printf("Área: ");
-scanf("%f", &area2); //area da cidade
-                                                                                                                          
-printf("PIB: ");
-scanf("%f", &PIB2); //PIB da cidade
+  printf("População: ");
+  scanf("%d", &populacao1); //populacao da cidade
 
-printf("Pontos turísticos: ");
-scanf("%d", &P_turistico2); //pontos turisticos
+  printf("Área: ");
+  scanf("%f", &area1); //area da cidade
+
+  printf("PIB: ");
+  scanf("%f", &PIB1); //PIB da cidade
+
+  printf("Pontos turísticos: ");
+  scanf("%d", &P_turistico1); //pontos turisticos
+
+  printf("\n"); //linha em branco para separar as cartas
+
+  //leitura dos dados da segunda carta
+  printf("segunda carta\n");
+  printf("Letra do estado: ");
+  scanf(" %c", &estado2); //letra do state
+
+  printf("Código do estado: ");
+  scanf("%s", codigo2); //codigo do estado
+
+  printf("Nome da cidade: ");
+  scanf("%s", nome_cidade2); //nome da cidade
+
+  printf("População: ");
+  scanf("%d", &populacao2); //populacao da cidade
+
+  printf("Área: ");
+  scanf("%f", &area2); //area da cidade
+
+  printf("PIB: ");
+  scanf("%f", &PIB2); //PIB da cidade
+
+  printf("Pontos turísticos: ");
+  scanf("%d", &P_turistico2); //pontos turisticos
                                                                                                                                     
-//calculos
-//calculos primeira carta
-densidade_populacional1 = (float)populacao1 / area1;
-PIB_per_capita1 = (PIB1 * 1000000000) / (float)populacao1;
+  //calculos
+  //calculos primeira carta
+  densidade_populacional1 = (float)populacao1 / area1;
+  PIB_per_capita1 = (PIB1 * 1000000000) / (float)populacao1;
                                                                                                                                             
-//calculos segunda carta
-densidade_populacional2 = (float)populacao2 / area2;
-PIB_per_capita2 = (PIB2 * 1000000000) / (float)populacao2;
+  //calculos segunda carta
+  densidade_populacional2 = (float)populacao2 / area2;
+  PIB_per_capita2 = (PIB2 * 1000000000) / (float)populacao2;
                                                                                                                                                     
-//superpoderes
-superpoder1 = (float)populacao1 + (float)P_turistico1 + area1 + PIB1 + densidade_populacional1 + PIB_per_capita1;
-superpoder2 = (float)populacao2 + (float)P_turistico2 + area2 + PIB2 + densidade_populacional2 + PIB_per_capita2;
+  //superpoderes
+  superpoder1 = (float)populacao1 + (float)P_turistico1 + area1 + PIB1 + densidade_populacional1 + PIB_per_capita1;
+  superpoder2 = (float)populacao2 + (float)P_turistico2 + area2 + PIB2 + densidade_populacional2 + PIB_per_capita2;
                                                                                                                                                           
-// Área para exibição dos dados da cidade
-//carta 1
-printf("\nCarta 1:\n");
-printf("Estado: %c\n", estado1);//letra do estado
-printf("Código: %c%s\n", estado1, codigo1);//codigo do estado
-printf("Nome da Cidade: %s\n", nome_cidade1);//nome da cidade
-printf("População: %d\n", populacao1);//populacao da cidade
-printf("Pontos Turísticos: %d\n", P_turistico1);//pontos turisticos
-printf("Área: %.2f km²\n", area1);//area da cidade
-printf("PIB: %.2f bilhões de reais\n", PIB1);//PIB da cidade
-printf("densidade populacional: %.2f hab/km²\n", densidade_populacional1);
-printf("PIB per capita: %.2f reais\n", PIB_per_capita1);
-printf("Superpoder: %.2f\n", superpoder1);
+  // Área para exibição dos dados da cidade
+  //carta 1
+  printf("\nCarta 1:\n");
+  printf("Estado: %c\n", estado1);//letra do estado
+  printf("Código: %c%s\n", estado1, codigo1);//codigo do estado
+  printf("Nome da Cidade: %s\n", nome_cidade1);//nome da cidade
+  printf("População: %d\n", populacao1);//populacao da cidade
+  printf("Pontos Turísticos: %d\n", P_turistico1);//pontos turisticos
+  printf("Área: %.2f km²\n", area1);//area da cidade
+  printf("PIB: %.2f bilhões de reais\n", PIB1);//PIB da cidade
+  printf("densidade populacional: %.2f hab/km²\n", densidade_populacional1);
+  printf("PIB per capita: %.2f reais\n", PIB_per_capita1);
+  printf("Superpoder: %.2f\n", superpoder1);
 
-printf("\n"); //linha em branco para separar as cartas
+  printf("\n"); //linha em branco para separar as cartas
 
-//carta 2
-printf("Carta 2:\n");
-printf("Estado: %c\n", estado2);//letra do estado
-printf("Código: %c%s\n", estado2, codigo2);//codigo do estado
-printf("Nome da Cidade: %s\n", nome_cidade2);//nome da cidade                                                                                                                                                                                                      printf("População: %d\n", populacao2);//populacao da cidade
-printf("Pontos Turísticos: %d\n", P_turistico2);//pontos turisticos
-printf("Área: %.2f km²\n", area2);//area da cidade
-printf("PIB: %.2f bilhões de reais\n", PIB2);//PIB da cidade
-printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);  
-printf("PIB per capita: %.2f reais\n", PIB_per_capita2);
-printf("Superpoder: %.2f\n", superpoder2);
+  //carta 2
+  printf("Carta 2:\n");
+  printf("Estado: %c\n", estado2);//letra do estado
+  printf("Código: %c%s\n", estado2, codigo2);//codigo do estado
+  printf("Nome da Cidade: %s\n", nome_cidade2);//nome da cidade                                                                                                                                                                                                      printf("População: %d\n", populacao2);//populacao da cidade
+  printf("Pontos Turísticos: %d\n", P_turistico2);//pontos turisticos
+  printf("Área: %.2f km²\n", area2);//area da cidade
+  printf("PIB: %.2f bilhões de reais\n", PIB2);//PIB da cidade
+  printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);  
+  printf("PIB per capita: %.2f reais\n", PIB_per_capita2);
+  printf("Superpoder: %.2f\n", superpoder2);
 
-//comparações
+  //comparações
 
-//comparação de apenas um atributo, este sendo escolhido pelo usuário
-printf("\nComparação:\n");
-printf("qual atributo deseja comparar?\n");
-printf("\n 1-população:\n2-Pontos turísticos: \n3-Área: \n4-PIB: \n5-Densidade populacional: \n6-PIB per capita\n7-Superpoder:\n");
-scanf("%d\n", &escolha_comparacao);
+  //comparação de apenas um atributo, este sendo escolhido pelo usuário
+  printf("\nComparação:\n");
+  printf("qual atributo deseja comparar?\n");
+  printf("1-população: \n");
+  printf("2-Pontos turísticos: \n");
+  printf("3-Área: \n");
+  printf("4-PIB: \n");
+  printf("5-Densidade populacional: \n");
+  printf("6-PIB per capita: \n");
+  printf("7-Superpoder: \n");
+  scanf("%d\n", &escolha_comparacao);
 
-if(escolha_comparacao == 1){
-  printf("população: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "carta 2 venceu");
-} 
-if(escolha_comparacao == 2){
+  //comparação usando switch case
+  switch (escolha_comparacao) {
+    
+    case 1: {
+    printf("população: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  } 
+    case 2: {
+    printf("Pontos turísticos: %s\n", (P_turistico1 > P_turistico2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  } 
+    case 3: {
+    printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  } 
+    case 4: {
+    printf("PIB: %s\n", (PIB1 > PIB2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  }
+    case 5: {
+    printf("Densidade populacional: %s\n", (densidade_populacional1 < densidade_populacional2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  }
+    case 6: {
+    printf("PIB per capita: %s\n", (PIB_per_capita1 > PIB_per_capita2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  }
+    case 7: {
+    printf("Superpoder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "carta 2 venceu");
+    break;
+  }
+  }
+
+  /* comparação com if/else
+  if(escolha_comparacao == 1){
+    printf("população: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "carta 2 venceu");
+  } 
+  if(escolha_comparacao == 2){
+    printf("Pontos turísticos: %s\n", (P_turistico1 > P_turistico2) ? "Carta 1 venceu" : "carta 2 venceu");
+  } 
+  if(escolha_comparacao == 3){
+    printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu" : "carta 2 venceu");
+  } 
+  if(escolha_comparacao == 4){
+    printf("PIB: %s\n", (PIB1 > PIB2) ? "Carta 1 venceu" : "carta 2 venceu");
+  }
+  if(escolha_comparacao == 5){
+    printf("Densidade populacional: %s\n", (densidade_populacional1 < densidade_populacional2) ? "Carta 1 venceu" : "carta 2 venceu");
+  }                                                                                                                                                                                                                                                                       if(escolha_comparacao == 6){
+    printf("PIB per capita: %s\n", (PIB_per_capita1 > PIB_per_capita2) ? "Carta 1 venceu" : "carta 2 venceu");
+  } 
+  if(escolha_comparacao == 7){
+    printf("Superpoder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "carta 2 venceu");
+  } */
+
+  //comparação geral de todos os atributos
+
+  /*printf("população: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "carta 2 venceu");
   printf("Pontos turísticos: %s\n", (P_turistico1 > P_turistico2) ? "Carta 1 venceu" : "carta 2 venceu");
-} 
-if(escolha_comparacao == 3){
   printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu" : "carta 2 venceu");
-} 
-if(escolha_comparacao == 4){
   printf("PIB: %s\n", (PIB1 > PIB2) ? "Carta 1 venceu" : "carta 2 venceu");
-}
-if(escolha_comparacao == 5){
   printf("Densidade populacional: %s\n", (densidade_populacional1 < densidade_populacional2) ? "Carta 1 venceu" : "carta 2 venceu");
-}                                                                                                                                                                                                                                                                       if(escolha_comparacao == 6){
   printf("PIB per capita: %s\n", (PIB_per_capita1 > PIB_per_capita2) ? "Carta 1 venceu" : "carta 2 venceu");
-} 
-if(escolha_comparacao == 7){
-  printf("Superpoder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "carta 2 venceu");
-} 
-//comparação geral de todos os atributos
-
-/*printf("população: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("Pontos turísticos: %s\n", (P_turistico1 > P_turistico2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("PIB: %s\n", (PIB1 > PIB2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("Densidade populacional: %s\n", (densidade_populacional1 < densidade_populacional2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("PIB per capita: %s\n", (PIB_per_capita1 > PIB_per_capita2) ? "Carta 1 venceu" : "carta 2 venceu");
-printf("Superpoder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "carta 2 venceu");*/
+  printf("Superpoder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "carta 2 venceu");*/
 
 return 0;
 } 
